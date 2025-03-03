1c66e8770fbcdebb091606158812565070541971
muhammad ikhsanudin <mikhsanudin2001@gmail.com>
2025-03-03 18:58:56 +0700

    Add Pallete Costume

---
+++ src/custome/TextField.java
@@ -0,0 +1,155 @@
+package custome;
+
+import java.awt.Color;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Insets;
+import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.geom.Rectangle2D;
+import javax.swing.JTextField;
+import javax.swing.Timer;
+import javax.swing.border.EmptyBorder;
+
+public class TextField extends JTextField {
+
+    public String getLabelText() {
+        return labelText;
+    }
+
+    public void setLabelText(String labelText) {
+        this.labelText = labelText;
+    }
+
+    public Color getLineColor() {
+        return lineColor;
+    }
+
+    public void setLineColor(Color lineColor) {
+        this.lineColor = lineColor;
+    }
+
+    private final Timer animator;
+    private boolean animateHinText = true;
+    private float location;
+    private boolean show;
+    private boolean mouseOver = false;
+    private String labelText = "Label";
+    private Color lineColor = new Color(3, 155, 216);
+
+    public TextField() {
+        setBorder(new EmptyBorder(20, 3, 10, 3));
+        setSelectionColor(new Color(76, 204, 255));
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent me) {
+                mouseOver = true;
+                repaint();
+            }
+
+            @Override
+            public void mouseExited(MouseEvent me) {
+                mouseOver = false;
+                repaint();
+            }
+        });
+        addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusGained(FocusEvent fe) {
+                showing(false);
+            }
+
+            @Override
+            public void focusLost(FocusEvent fe) {
+                showing(true);
+            }
+        });
+
+        animator = new Timer(10, new ActionListener() {
+            private int step = 0;
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                location = step / 30f; // Total 30 steps
+                repaint();
+                step++;
+                if (step > 30) {
+                    animator.stop();
+                }
+            }
+        });
+    }
+
+    private void showing(boolean action) {
+        show = action;
+        animator.start();
+    }
+
+    @Override
+    public void paint(Graphics grphcs) {
+        super.paint(grphcs);
+        Graphics2D g2 = (Graphics2D) grphcs;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_LCD_HRGB);
+        int width = getWidth();
+        int height = getHeight();
+        if (mouseOver) {
+            g2.setColor(lineColor);
+        } else {
+            g2.setColor(new Color(150, 150, 150));
+        }
+        g2.fillRect(2, height - 1, width - 4, 1);
+        createHintText(g2);
+        createLineStyle(g2);
+        g2.dispose();
+    }
+
+    private void createHintText(Graphics2D g2) {
+        Insets in = getInsets();
+        g2.setColor(new Color(150, 150, 150));
+        FontMetrics ft = g2.getFontMetrics();
+        Rectangle2D r2 = ft.getStringBounds(labelText, g2);
+        double height = getHeight() - in.top - in.bottom;
+        double textY = (height - r2.getHeight()) / 2;
+        double size;
+        if (animateHinText) {
+            if (show) {
+                size = 18 * (1 - location);
+            } else {
+                size = 18 * location;
+            }
+        } else {
+            size = 18;
+        }
+        g2.drawString(labelText, in.right, (int) (in.top + textY + ft.getAscent() - size));
+    }
+
+    private void createLineStyle(Graphics2D g2) {
+        if (isFocusOwner()) {
+            double width = getWidth() - 4;
+            int height = getHeight();
+            g2.setColor(lineColor);
+            double size;
+            if (show) {
+                size = width * (1 - location);
+            } else {
+                size = width * location;
+            }
+            double x = (width - size) / 2;
+            g2.fillRect((int) (x + 2), height - 2, (int) size, 2);
+        }
+    }
+
+    @Override
+    public void setText(String string) {
+        if (!getText().equals(string)) {
+            showing(string.equals(""));
+        }
+        super.setText(string);
+    }
+}
